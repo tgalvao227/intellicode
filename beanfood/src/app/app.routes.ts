ef9d8a448d9ec8b7342efec29d7101b761a389e4
tgalvao227 <54628152+tgalvao227@users.noreply.github.com>
2024-05-17 16:45:29 -0300

    Add files via upload

--- beanfood/src/app/app.routes.ts
+++ beanfood/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 import { HomeComponent } from './home/home.component';
+import { CompraComponent } from './compra/compra.component';
+import { FinalizarCompraComponent } from './finalizar-compra/finalizar-compra.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: RegistroComponent },
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'compra', component: CompraComponent },
+    { path: 'finalizar-compra', component: FinalizarCompraComponent },
+    { path: 'sobre', component: SobreComponent }
   ];
