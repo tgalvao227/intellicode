0c00a3dc8228e33dafa2108cc282342203eebafb
tgalvao227 <54628152+tgalvao227@users.noreply.github.com>
2024-05-17 17:38:44 -0300

    Add files via upload

--- beanfood/src/app/home/home.module.ts
+++ beanfood/src/app/home/home.module.ts
@@ -3,14 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon'
 import { HomeComponent } from './home.component';
-import { HeaderModule } from '../header/header.module';
 @NgModule({
   imports: [
     CommonModule,
     MatCardModule,
     HomeComponent,
-    MatIconModule,
-    HeaderModule
+    MatIconModule
+  
   ]
 })
 export class HomeModule { }
