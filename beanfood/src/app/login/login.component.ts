b05eb282f3dee06868cc10f4d2944f9032a7a4b3
tgalvao227 <54628152+tgalvao227@users.noreply.github.com>
2024-05-17 14:54:12 -0300

    Add files via upload

--- beanfood/src/app/login/login.component.ts
+++ beanfood/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RegistroComponent } from '../registro/registro.component';
 import { HomeComponent } from '../home/home.component';
 import { MatCard } from '@angular/material/card';
@@ -12,8 +13,25 @@ import { FooterComponent } from "../footer/footer.component";
     standalone: true,
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
-    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HomeComponent, RegistroComponent, HeaderComponent, FooterComponent]
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HomeComponent, RegistroComponent, HeaderComponent, FooterComponent, ReactiveFormsModule]
 })
 export class LoginComponent {
+    loginForm: FormGroup;
 
-}
+    constructor(private fb: FormBuilder) {
+      this.loginForm = this.fb.group({
+        email: [''],
+        password: [''],
+      })
+    }
+  
+    onSubmit() {
+      if (this.loginForm.valid) {
+        console.log('Form Submitted', this.loginForm.value);
+      } else {
+        console.log('Form is not valid');
+      }
+    }
+  }
+  
+
