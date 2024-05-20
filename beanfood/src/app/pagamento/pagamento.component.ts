e9dcc1859557b45daa1e5069c4cbe6ae9adb5ddd
tgalvao227 <54628152+tgalvao227@users.noreply.github.com>
2024-05-19 22:18:53 -0300

    Add files via upload

--- beanfood/src/app/pagamento/pagamento.component.ts
+++ beanfood/src/app/pagamento/pagamento.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { PagamentoService } from './pagamento.service';
 
 @Component({
   selector: 'app-pagamento',
@@ -19,4 +20,17 @@ export class PagamentoComponent {
     this.formaPagamento = paymentMethod;
   }
 
+  //dadosPagamento: any DEPOIS COLOCAR DENTRO DO PARENTESES
+  realizarPagamento() {
+  //   this.pagamentoService.realizarPagamento(dadosPagamento).subscribe(
+  //     response => {
+  //       console.log('Pagamento realizado com sucesso:', response);
+  //       // Faça algo com a resposta, como redirecionar para a página de confirmação de pagamento
+  //     },
+  //     error => {
+  //       console.error('Erro ao realizar o pagamento:', error);
+  //       // Trate o erro, por exemplo, mostrando uma mensagem de erro para o usuário
+  //     }
+  //   );
+   }
 }
