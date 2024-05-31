b8c2e7860b079fd84fb019bc795782c2e2e614d1
tgalvao227 <54628152+tgalvao227@users.noreply.github.com>
2024-05-31 19:19:17 -0300

    Add files via upload

--- beanfood/src/app/pedido-personalizado/pedido-personalizado.component.ts
+++ beanfood/src/app/pedido-personalizado/pedido-personalizado.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
-
+import { CarrinhoService, PedidoPersonalizado, Itens } from '../carrinho/carrinho.service';
+import { Router } from '@angular/router';
 
 interface Item {
   nome: string;
   preco: number;
+  imagem: string;
 }
 
 @Component({
@@ -19,34 +21,36 @@ interface Item {
 })
 export class PedidoPersonalizadoComponent {
   proteinas: Item[] = [
-    { nome: 'Patinho Desfiado', preco: 14.50 },
-    { nome: 'Patinho Moído', preco: 14.50 },
-    { nome: 'Frango Grelhado', preco: 12.50 },
-    { nome: 'Ovo Cozido', preco: 3.00 },
-    { nome: 'Peixe Assado', preco: 16.00 },
-    { nome: 'Peixe Grelhado', preco: 16.00 }
+    { nome: 'Patinho Desfiado', preco: 14.50, imagem: 'assets/patinho_desfiado.jpg' },
+    { nome: 'Patinho Moído', preco: 14.50, imagem: 'assets/patinho_moido.jpg' },
+    { nome: 'Frango Grelhado', preco: 12.50, imagem: 'assets/frango-grelhado.jpeg' },
+    { nome: 'Ovo Cozido', preco: 3.00, imagem: 'assets/ovo-cozido.jpg' },
+    { nome: 'Peixe Assado', preco: 16.00, imagem: 'assets/peixe-assado.jpg' },
+    { nome: 'Peixe Grelhado', preco: 16.00, imagem: 'assets/peixe-grelhado.jpg' }
   ];
   
-
   acompanhamentos: Item[] = [
-    { nome: 'Abóbora Assada', preco: 7.30 },
-    { nome: 'Abobrinha Refogada Orgânica', preco: 6.40 },
-    { nome: 'Arroz 7 Grãos Cozido', preco: 6.40 },
-    { nome: 'Arroz à Grega', preco: 6.40 },
-    { nome: 'Arroz Branco Cozido', preco: 6.40 },
-    { nome: 'Arroz com Brócolis', preco: 7.30 },
-    { nome: 'Arroz Integral Cozido', preco: 6.40 }
+    { nome: 'Abóbora Assada', preco: 7.30, imagem: 'assets/abobora.jpg' },
+    { nome: 'Abobrinha Refogada Orgânica', preco: 6.40, imagem: 'assets/abobrinha-refogada.jpeg' },
+    { nome: 'Arroz 7 Grãos Cozido', preco: 6.40, imagem: 'assets/arroz-7.png' },
+    { nome: 'Arroz à Grega', preco: 6.40, imagem: 'assets/arroz-a-grega-02.jpg' },
+    { nome: 'Arroz Branco Cozido', preco: 6.40, imagem: 'assets/arroz-branco.jpg' },
+    { nome: 'Arroz com Brócolis', preco: 7.30, imagem: 'assets/arroz-brocolis.jpg' },
+    { nome: 'Arroz Integral Cozido', preco: 6.40, imagem: 'assets/arroz-integral.jpg' }
   ];
 
-  pedido: {
-    proteina: Item | null,
-    acompanhamentos: Item[]
-  } = {
+
+  pedido: PedidoPersonalizado = {
     proteina: null,
     acompanhamentos: []
-  };
+};
 
   observacao: string = '';
+
+  constructor(
+    private carrinhoService: CarrinhoService,
+    private router: Router
+  ) {}
 
   selecionarProteina(item: Item) {
     this.pedido.proteina = item;
@@ -72,7 +76,13 @@ export class PedidoPersonalizadoComponent {
   }
 
   finalizarPedido() {
-    // Implementar lógica para finalizar pedido
-    console.log('Pedido finalizado:', this.pedido, 'Observação:', this.observacao);
+    const pedidoPersonalizado: PedidoPersonalizado = {
+      proteina: this.pedido.proteina,
+      acompanhamentos: this.pedido.acompanhamentos
+    };
+    this.carrinhoService.setPedidoPersonalizado(pedidoPersonalizado);
+    console.log(pedidoPersonalizado)
+    console.log('Pedido finalizado:', pedidoPersonalizado, 'Observação:', this.observacao);
+    this.router.navigate(['/finalizar-compra']);
   }
 }
