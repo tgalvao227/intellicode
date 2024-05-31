69a5915f13edcfc2f8b0dabf05ff678769735322
tgalvao227 <54628152+tgalvao227@users.noreply.github.com>
2024-05-31 19:20:22 -0300

    Add files via upload

--- beanfood/src/app/home/home.component.ts
+++ beanfood/src/app/home/home.component.ts
@@ -2,37 +2,36 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
-import { RegistroComponent } from '../registro/registro.component';
 import { HomeModule } from './home.module';
 import { MatCard } from '@angular/material/card';
 import { ElementRef, Renderer2, ViewChild } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
 
 interface Testimonial {
   content: string;
   author: string;
 }
 
-interface Promocao {
+export interface Itens{
+  id: number;
   nome: string;
   imagem: string;
   preco: string;
   quantidade: number;
 }
 
-
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, LoginComponent, HomeComponent, RegistroComponent, HeaderComponent, FooterComponent]
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, LoginComponent, HomeComponent, FooterComponent, HeaderComponent]
 })
 
 export class HomeComponent implements OnInit {
   isMarmitas: boolean = true;
-  sectionTitle: string = 'Delícias prontas para o seu dia a dia'; // Adicionando a propriedade sectionTitle
+  sectionTitle: string = 'Delícias prontas para o seu dia a dia'; 
   testimonials: Testimonial[] = [
     { content: 'As marmitas da BeanFood são deliciosas e me ajudam a manter uma alimentação saudável mesmo nos dias corridos.', author: 'Maria Souza' },
     { content: 'Adoro as opções de dietas da BeanFood! Elas são práticas, saborosas e me ajudaram a perder peso de forma saudável.', author: 'Pedro Santos' },
@@ -40,13 +39,14 @@ export class HomeComponent implements OnInit {
   ];
   currentTestimonialIndex: number = 0;
 
-  promocoes: Promocao[] = [
-    { nome: 'Salmão com molho', imagem: 'assets/salmao.jpg', preco: 'R$ 20,00', quantidade: 1 },
-    { nome: 'Bruschetta sem glúten', imagem: 'assets/italia.jpg', preco: 'R$ 15,00', quantidade: 1 },
-    { nome: 'Panqueca de carne', imagem: 'assets/panqueca.jpg', preco: 'R$ 18,00', quantidade: 1 },
-    { nome: 'Arroz e salmão', imagem: 'assets/peixe.png', preco: 'R$ 25,00', quantidade: 1 },
-    { nome: 'Salada com grãos', imagem: 'assets/pratosalada.jpg', preco: 'R$ 22,00', quantidade: 1 },
-    { nome: 'Feijoada vegana', imagem: 'assets/feijoada.jpg', preco: 'R$ 30,00', quantidade: 1 },
+
+  promocoes: Itens[] = [
+    { nome: 'Salmão com molho', imagem: 'assets/salmao.jpg', preco: 'R$ 20,00', quantidade: 1, id: 4 },
+    { nome: 'Bruschetta sem glúten', imagem: 'assets/italia.jpg', preco: 'R$ 15,00', quantidade: 1, id: 5 },
+    { nome: 'Panqueca de carne', imagem: 'assets/panqueca.jpg', preco: 'R$ 18,00', quantidade: 1, id: 6 },
+    { nome: 'Arroz e salmão', imagem: 'assets/peixe.png', preco: 'R$ 25,00', quantidade: 1, id: 7 },
+    { nome: 'Salada com grãos', imagem: 'assets/pratosalada.jpg', preco: 'R$ 22,00', quantidade: 1, id: 8 },
+    { nome: 'Feijoada vegana', imagem: 'assets/feijoada.jpg', preco: 'R$ 30,00', quantidade: 1, id: 9},
   ];
 
   toggleSection() {
@@ -71,19 +71,19 @@ export class HomeComponent implements OnInit {
     this.currentTestimonialIndex = (this.currentTestimonialIndex + 1) % this.testimonials.length;
   }
   
-  incrementQuantity(promocao: Promocao) {
-    promocao.quantidade++;
+  incrementQuantity(itens: Itens) {
+    itens.quantidade++;
   }
 
   // Função para decrementar a quantidade do item, com verificação para garantir que não seja menor que 1
-  decrementQuantity(promocao: Promocao) {
-    if (promocao.quantidade > 1) {
-      promocao.quantidade--;
+  decrementQuantity(itens: Itens) {
+    if (itens.quantidade > 1) {
+      itens.quantidade--;
     }
   }
 
   // Função para adicionar o item ao carrinho 
-  addToCart(promocao: Promocao) {
+  addToCart(itens: Itens) {
     
   }
 
