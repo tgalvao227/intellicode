855333e0f9a30ba24d0711a431cb6fd6f3b4418f
tgalvao227 <54628152+tgalvao227@users.noreply.github.com>
2024-05-31 19:21:03 -0300

    Add files via upload

--- beanfood/src/app/registro/registro.component.ts
+++ beanfood/src/app/registro/registro.component.ts
@@ -1,56 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-// import { RegistroService } from './registro.service';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  standalone: true,
-  imports: [HeaderComponent, FooterComponent, CommonModule, ReactiveFormsModule],
-  styleUrls: ['./registro.component.scss']
+    selector: 'app-registro',
+    standalone: true,
+    templateUrl: './registro.component.html',
+    styleUrls: ['./registro.component.scss'],
+    imports: [FooterComponent, HeaderComponent, CommonModule, ReactiveFormsModule]
 })
 export class RegistroComponent implements OnInit {
-  registroForm!: FormGroup;
+    registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private http: HttpClient, ) {} //INSERIR O PRIVATE DO SERVICE
+    constructor(
+        private fb: FormBuilder,
+        private http: HttpClient,
+        private router: Router
+    ) {
+        this.registerForm = this.fb.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(8)]],
+            profile: ['', Validators.required],
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            cpf: ['', [Validators.required, Validators.pattern(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)]],
+            gender: ['', Validators.required],
+            birthdate: ['', Validators.required],
+            phone: ['', [Validators.required, Validators.pattern(/^\(\d{2}\) \d{4,5}-\d{4}$/)]],
+            cep: ['', [Validators.required, Validators.pattern(/^\d{5}-\d{3}$/)]],
+            newsletter: [false],
+            privacyPolicy: [false, Validators.requiredTrue]
+        });
+    }
 
-  ngOnInit(): void {
-    this.registroForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      profile: ['Pessoa Física', Validators.required],
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      cpf: ['', [Validators.required, Validators.pattern(/\d{3}\.\d{3}\.\d{3}-\d{2}/)]],
-      gender: ['', Validators.required],
-      birthdate: ['', Validators.required],
-      phone: ['', [Validators.required, Validators.pattern(/\(\d{2}\) \d{4,5}-\d{4}/)]],
-      cep: ['', [Validators.required, Validators.pattern(/\d{5}-\d{3}/)]],
-      newsletter: [false],
-      privacyPolicy: [false, Validators.requiredTrue]
-    });
-  }
+    ngOnInit(): void {}
 
-  onSubmit() {
-  //   if (this.registroForm.valid) {
-  //     const userData = this.registroForm.value;
+    onSubmit(): void {
+        if (this.registerForm.valid) {
+            const formData = this.registerForm.value;
+            console.log(formData);
 
-  //     this.registroService.registrarUsuario(userData).subscribe(
-  //       response => {
-  //         console.log('Usuário registrado com sucesso:', response);
-          
-  //       },
-  //       error => {
-  //         console.error('Erro ao registrar usuário:', error);
-         
-  //       }
-  //     );
-  //   } else {
-  //     console.log('Formulário inválido. Verifique os campos.');
-  //   }
-  }
+            // Substitua 'URL_DA_API' pela URL real da sua API
+            this.http.post('URL_DA_API', formData).subscribe(response => {
+                console.log('Cadastro realizado com sucesso!', response);
+                this.router.navigate(['/login']);
+            }, error => {
+                console.error('Erro ao realizar o cadastro', error);
+            });
+        } else {
+            console.log('Formulário inválido');
+        }
+    }
 }
