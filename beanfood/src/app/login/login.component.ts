f365080520281762f8d203019b10bcfdb2e80124
tgalvao227 <54628152+tgalvao227@users.noreply.github.com>
2024-05-31 19:17:40 -0300

    Add files via upload

--- beanfood/src/app/login/login.component.ts
+++ beanfood/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 // import { LoginService } from './login.service';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -8,33 +10,45 @@ import { FooterComponent } from '../footer/footer.component';
   selector: 'app-login',
   templateUrl: './login.component.html',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [
+    HeaderComponent, 
+    FooterComponent, 
+    CommonModule, 
+    ReactiveFormsModule 
+  ],
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    // private loginService: LoginService,
+    private router: Router
+  ) {
     this.loginForm = this.fb.group({
-      email: [''],
-      password: [''],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
   onSubmit() {
-    // if (this.loginForm.valid) {
-    //   const credentials = this.loginForm.value;
+    if (this.loginForm.valid) {
+      const credentials = this.loginForm.value;
 
-    //   this.loginService.login(credentials).subscribe(
-    //     response => {
-    //       console.log('Login bem-sucedido:', response);
-    //     },
-    //     error => {
-    //       console.error('Erro no login:', error);
-    //     }
-    //   );
-    // } else {
-    //   console.log('Formulário inválido. Verifique os campos.');
-    // }
-  }
+  //     this.loginService.login(credentials).subscribe(
+  //       response => {
+  //         if (response) {
+  //           console.log('Login bem-sucedido:', response);
+  //           // this.router.navigate(['/']); 
+  //         } else {
+  //           console.error('Credenciais inválidas');
+  //         }
+  //       },
+  //       error => {
+  //         console.error('Erro no login:', error);
+  //       }
+  //     );
+     }
+   }
 }
--- beanfood/src/app/login/login.service.ts
+++ beanfood/src/app/login/login.service.ts
@@ -1,16 +1,26 @@
 // import { Injectable } from '@angular/core';
 // import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
+// import { Observable, of } from 'rxjs';
+// import { catchError, map } from 'rxjs/operators';
 
 // @Injectable({
 //   providedIn: 'root'
 // })
 // export class LoginService {
-//   private apiUrl = '';
 
-//   constructor(private http: HttpClient) { }
+//   private apiUrl = 'URL_DA_API';
 
-//   login(credentials: any): Observable<any> {
-//     return this.http.post<any>(this.apiUrl, credentials);
+//   constructor(private http: HttpClient) {}
+
+//   login(credentials: { email: string, password: string }): Observable<any> {
+//     return this.http.post(this.apiUrl, credentials).pipe(
+//       map(response => {
+//         return response;
+//       }),
+//       catchError(error => {
+//         console.error('Erro ao fazer login', error);
+//         return of(null);
+//       })
+//     );
 //   }
-// }
+//   }
