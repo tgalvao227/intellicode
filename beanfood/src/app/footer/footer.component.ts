978c72617ae9d968f470ce15e396b212f8f8ba1f
tgalvao227 <54628152+tgalvao227@users.noreply.github.com>
2024-06-08 20:12:36 -0300

    Add files via upload

--- beanfood/src/app/footer/footer.component.ts
+++ beanfood/src/app/footer/footer.component.ts
@@ -4,12 +4,10 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { HomeComponent } from '../home/home.component';
 import { LoginComponent } from '../login/login.component';
-import { RegistroComponent } from '../registro/registro.component';
-
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, LoginComponent, HomeComponent, RegistroComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, LoginComponent, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
--- beanfood/src/app/header/header.component.ts
+++ beanfood/src/app/header/header.component.ts
@@ -3,12 +3,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { LoginComponent } from '../login/login.component';
-import { RegistroComponent } from '../registro/registro.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, LoginComponent, HomeComponent, RegistroComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, LoginComponent, HomeComponent, HeaderComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
